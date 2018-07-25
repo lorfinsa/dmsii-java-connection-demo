d5676d8621a7fcdc80d99ae8970f04ec61729ae7
Jonathan Buzzetti <buzzetti.jp@gmail.com>
2018-07-25 12:15:55 -0300

    Init

---
+++ src/main/java/com/example/demo/DmsiiConnectionDemoApplication.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class DmsiiConnectionDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmsiiConnectionDemoApplication.class, args);
+    }
+
+    @Component
+    public static class DataLoader implements CommandLineRunner {
+
+        @Autowired
+        private JdbcTemplate jdbcTemplate;
+        
+        @Value("${db.perfil}")
+        private String perfil;
+
+        @Override
+        public void run(String... strings) throws Exception {
+            System.out.println(jdbcTemplate.queryForList("SELECT TOP 3 * FROM " + perfil + ".PASDO001"));
+        }
+    }
+
+}
